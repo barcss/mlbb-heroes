d824f0590cd4ef1162782a224b40b88c53e2bdb4
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-09-08 23:35:08 +0800

    handle function through controller and remove hard coded data

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
+use App\Http\Controllers\HeroController;
+use App\Models\Hero;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/heroes', function() {
-    $heroes = [
-        ["name" => "Beatrix", "role" => "Marksman", "id" => "40", 'price' => '32000'],
-        ["name" => "Guinevere", "role" => "Fighter", "id" => "12", 'price' => '24000'],
-    ];
-    return view('heroes.index', ['greetings'=>"SEX", "heroes" => $heroes]);
-});
+Route::get('/heroes', [HeroController::class, 'index']);
 
 Route::get('/heroes/add', function() {
     return view('heroes.add');
