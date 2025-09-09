334d47f1f35c382ef5cc727a5ce8be7fbb03c12f
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-09-09 23:51:23 +0800

    change table schema

--- database/migrations/2025_09_06_185508_create_heroes_table.php
+++ database/migrations/2025_09_06_185508_create_heroes_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('heroes', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('price');
+            $table->string('role');
+            $table->string('skills');
             $table->timestamps();
         });
     }
