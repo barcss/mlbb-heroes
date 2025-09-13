d13b09359d146a353992a30fde494d235582ca57
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-09-13 23:26:36 +0800

    add pagination pre made links in hero list

--- resources/views/heroes/index.blade.php
+++ resources/views/heroes/index.blade.php
@@ -2,9 +2,10 @@
     <h2 class="text-3xl">MLBB HEROES</h2>
     
     @foreach($heroes as $hero) 
-   
             <x-card href="{{ route('heroes.view', $hero->id) }}" :highlight="$hero['price'] < 32000">
                 <h3> {{ $hero['name'] }}</h3>
             </x-card>
     @endforeach
+
+    {{ $heroes->links() }}
 </x-layout>
