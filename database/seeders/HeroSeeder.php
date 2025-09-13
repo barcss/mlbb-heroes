5af4123034d584a997844b898866fad94adf289f
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-09-13 23:26:13 +0800

    Create new 20 rows of Hero in seeders

--- database/seeders/HeroSeeder.php
+++ database/seeders/HeroSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Hero;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class HeroSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Hero::factory()->count(20)->create();
     }
 }
