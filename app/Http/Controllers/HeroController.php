ec86ec253985113841657dd95fd99d7bdd08f43e
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-09-13 23:23:47 +0800

    Paginate heroes query

--- app/Http/Controllers/HeroController.php
+++ app/Http/Controllers/HeroController.php
@@ -9,7 +9,7 @@ class HeroController extends Controller
 {
     //Retrieve all Hero at order then return a view with data of heroes
     public function index() {
-        $heroes = Hero::orderBy('created_at', 'desc')->get();
+        $heroes = Hero::orderBy('created_at', 'desc')->paginate(5);
         return view('heroes.index', ["heroes" => $heroes]);
     }
 
